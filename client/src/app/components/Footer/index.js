93de10acc5363ab516a503829ed3998778ef9767
Jack <vomacka@usc.edu>
2021-04-24 00:20:23 -0500

    Footer update 2

--- client/src/app/components/Footer/index.js
+++ client/src/app/components/Footer/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./index.scss";
 
 const Footer = () => {
     return (
         <footer className="Footer">
             <div className="inner container is-fixed">
-                <a className="no-deco" href="/tos" rel="noopener noreferrer" >terms of service</a>
-                <a className="no-deco" href="/about" rel="noopener noreferrer" >any questions?</a>
-                <a className="no-deco" href="/privacy" rel="noopener noreferrer" >privacy policy</a>
+                <React.Fragment>
+                    <NavLink exact activeClassName="active" className="no-deco" to="/tos">
+                        terms of service
+                    </NavLink>
+                    <NavLink exact activeClassName="active" className="no-deco" to="/about">
+                        any questions?
+                    </NavLink>
+                    <NavLink exact activeClassName="active" className="no-deco" to="/privacy">
+                        privacy policy
+                    </NavLink>
+                </React.Fragment>
                 <p>© SeniorSCramble 2021 </p>
             </div>
         </footer>
