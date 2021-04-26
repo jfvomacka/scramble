3edfed176e16574aa43e7cdeb3517414bde61cf4
Jack <vomacka@usc.edu>
2021-04-25 23:08:41 -0500

    Demo version, no fonts locally

--- client/src/app/components/Footer/index.js
+++ client/src/app/components/Footer/index.js
@@ -6,13 +6,6 @@ const Footer = () => {
     return (
         <footer className="Footer">
             <div className="inner container is-fixed">
-<<<<<<< HEAD
-                <a className="no-deco" href="/TOS" rel="noopener noreferrer" >Terms of Service</a>
-                <a className="no-deco" href="/about" rel="noopener noreferrer" >FAQ</a>
-                <a className="no-deco" href="/Privacy" rel="noopener noreferrer" >Privacy Policy</a>
-                </div>
-               <div className="inner container is-fixed"> <p>© SeniorSCramble 2021 </p>
-=======
                 <React.Fragment>
                     <NavLink exact activeClassName="active" className="no-deco" to="/tos">
                         terms of service
@@ -25,7 +18,6 @@ const Footer = () => {
                     </NavLink>
                 </React.Fragment>
                 <p>© SeniorSCramble 2021 </p>
->>>>>>> 92f3182161db8e5ff1c19a2e216e2c65e435fcbb
             </div>
         </footer>
     )
