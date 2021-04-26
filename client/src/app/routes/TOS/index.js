8c966d11966345cc8fdf4c41611c9c589ecd6669
poptropicam <rudyfalagan@gmail.com>
2021-04-26 10:53:32 -0700

    ok

--- client/src/app/routes/TOS/index.js
+++ client/src/app/routes/TOS/index.js
@@ -6,9 +6,107 @@ const TOS = () => {
   return (
     <div className="TOS">
       <div className="inner container is-fluid">
-        <h2>Terms Of Service</h2>
-        
-      </div>
+        <div className="title">TERMS &nbsp; OF &nbsp; SERVICE</div>
+        <p>&nbsp;</p>
+        <h3>1. Acceptance of Terms of Use Agreement. </h3>
+<p>By creating a Senior Scramble account or by using any Senior Scramble service, whether through a mobile device or computer (collectively, the “Site”) you agree to be bound by (i) these Terms of Use, and (ii) our Privacy Policy, both of which are incorporated by reference into this Agreement. If you do not accept and agree to be bound by all of the terms of this Agreement, you should not use the Site.
+</p>
+<p>We may make changes to this Agreement and to the Site from time to time. We may do this for a variety of reasons including to reflect changes in or requirements of the law, new features, or changes in business practices. The most recent version of this Agreement will be posted on the Site under the page Terms of Service, and you should regularly check for the most recent version. The most recent version is the version that applies. If the changes include material changes that affect your rights or obligations, we will notify you in advance of the changes by reasonable means, which could include notification through the Site or via email. If you continue to use the Site after the changes become effective, then you agree to the revised Agreement. You agree that this Agreement shall supersede any prior agreements (except as specifically stated herein), and shall govern your entire relationship with Senior Scramble, including but not limited to events, agreements, and conduct preceding your acceptance of this Agreement.
+</p>
+<h3>2. Eligibility. </h3>
+<p>You must be at least 18 years of age and have a USC email account to create an account on Senior Scramble and use the Site. By creating an account and using the Site, you represent and warrant that: 
+</p><p>You can form a binding contract with Senior Scramble.
+</p><p>You are not a person who is barred from using the Site under the laws of the United States or any other applicable jurisdiction–meaning that you do not appear on the U.S. Treasury Department’s list of Specially Designated Nationals or face any other similar prohibition.
+</p><p>You will comply with this Agreement and all applicable local, state, national and international laws, rules and regulations.
+</p><p>You have never been convicted of or pled no contest to a felony, a sex crime, or any crime involving violence, and that you are not required to register as a sex offender with any state, federal or local sex offender registry
+<p>&nbsp;</p>
+<h3>3. Your Account. </h3>
+<p>In order to use the Site you will be asked to provide us with certain personally identifiable information that can be used to contact or identify you. For more information regarding the information we collect from you and how we use it, please consult our Privacy Policy. 
+
+</p><p>You are responsible for maintaining the confidentiality of your login credentials you use to sign up for Senior Scramble, and you are solely responsible for all activities that occur under those credentials. If you think someone has gained access to your account, please immediately contact us.
+</p>
+<p>&nbsp;</p>
+<h3>4. Modifying the Service and Termination. </h3>
+<p>Senior Scramble we may add new product features or enhancements from time to time as well as remove some features, and if these actions do not materially affect your rights or obligations, we may not provide you with notice before taking them. We may even suspend the Site entirely, in which event we will notify you in advance unless extenuating circumstances, such as safety or security concerns, prevent us from doing so. 
+</p>
+<p>You may terminate your account at any time, for any reason, by following the instructions on the profile page of the Site. 
+</p>
+<p>Senior Scramble may terminate your account at any time without notice if it believes that you have violated this Agreement. Upon such termination, you will not be entitled to an explanation as to why your account was terminated or the ability to appeal your termination. After your account is terminated, this Agreement will terminate, except that the following provisions will still apply to you and Senior Scramble: Section 4, Section 5, and Sections 10 and 11.
+</p>
+<p>&nbsp;</p>
+<h3>5. Safety; Your Interactions with Other Members.</h3> 
+<p>Senior Scramble strives to encourage a respectful member experience through features like the double opt-in that allows members to share methods of communication only after they have both indicated interest in one another, it is not responsible for the conduct of any member on or off of the Site. You agree to use caution in all interactions with other members, particularly if you decide to communicate off the Site or meet in person.
+</p>
+<p><b>YOU ARE SOLELY RESPONSIBLE FOR YOUR INTERACTIONS WITH OTHER MEMBERS. YOU UNDERSTAND THAT SENIOR SCRAMBLE DOES NOT CONDUCT CRIMINAL BACKGROUND CHECKS ON ITS MEMBERS OR OTHERWISE INQUIRE INTO THE BACKGROUND OF ITS MEMBERS. SENIOR SCRAMBLE MAKES NO REPRESENTATIONS OR WARRANTIES AS TO THE CONDUCT OF MEMBERS.</b></p>
+<p>&nbsp;</p>
+<h3>6. Rights Senior Scramble Grants You. </h3>
+<p>Senior Scramble grants you a personal, free, nonexclusive, and non-sublicensable license to access and use the Site. This license is for the sole purpose of letting you use and enjoy the Site’s benefits as intended by Senior Scramble and permitted by this Agreement. Therefore, you agree not to:</p>
+<ul>
+<li>Express or imply that any statements you make are endorsed by Senior Scramble. 
+</li><li>Use any robot, bot, spider, crawler, scraper, site search/retrieval application, proxy or other manual or automatic device, method or process to access, retrieve, index, “data mine,” or in any way reproduce or circumvent the navigational structure or presentation of the Site or its contents.
+</li><li>Use the Site in any way that could interfere with, disrupt or negatively affect the Site or the servers or networks connected to the Site. 
+</li><li>Upload viruses or other malicious code or otherwise compromise the security of the Site. 
+</li><li>Forge headers or otherwise manipulate identifiers in order to disguise the origin of any information transmitted to or through the Site. 
+</li><li>“Frame” or “mirror” any part of the Site without Senior Scramble prior written authorization. 
+</li><li>Use meta tags or code or other devices containing any reference to Senior Scramble or the Site (or any trade name, service mark, logo or slogan of Senior Scramble) to direct any person to any other website for any purpose. 
+</li><li>Modify, adapt, sublicense, translate, sell, reverse engineer, decipher, decompile or otherwise disassemble any portion of the Site, or cause others to do so. 
+</li><li>Use or develop any third-party applications that interact with the Site or other members’ Content or information without our written consent. 
+</li><li>Use, access, or publish the Senior Scramble programming interface without our written consent.
+</li><li>Probe, scan or test the vulnerability of our Site or any system or network.
+</li><li>Encourage or promote any activity that violates this Agreement.</li></ul>
+<p>Senior Scramble may investigate and take any available legal action in response to illegal and/ or unauthorized uses of the Site, including termination of your account. 
+</p>
+<p>&nbsp;</p>
+<h3>7. Rights you Grant Senior Scramble.</h3>
+<p>By creating an account, you grant to Senior Scramble a worldwide, transferable, sub-licensable, royalty-free, right and license to host, store, use, copy, display, reproduce, adapt, edit, publish, modify and distribute any information you post, upload, display or otherwise make available (collectively, “post”) on the Site. Senior Scramble’s license to your Content shall be non-exclusive, except that Senior Scramble’s license shall be exclusive with respect to derivative works created through use of the Site. For example, Senior Scramble would have an exclusive license to screenshots of the Site that include your Content. In addition, so that Senior Scramble can prevent the use of your Content outside of the Site, you authorize Senior Scramble to act on your behalf with respect to infringing uses of your Content taken from the Site by other members or third parties. This expressly includes the authority, but not the obligation, to send notices pursuant to 17 U.S.C. § 512(c)(3) (i.e., DMCA Takedown Notices) on your behalf if your Content is taken and used by third parties outside of the Site. Our license to your Content is subject to your rights under applicable law (for example laws regarding personal data protection to the extent any Content contains personal information as defined by those laws) and is for the limited purpose of operating, developing, providing, and improving the Site and researching and developing new ones. You agree that any Content you place or that you authorize us to place on the Site may be viewed by other members and may be viewed by any person visiting or participating in the Site (such as individuals who may receive shared Content from other Senior Scramble members).
+
+</p><p>You agree that all information that you submit upon creation of your account is accurate and truthful and you have the right to post the Content on the Site and grant the license to Senior Scramble above.
+
+</p><p>You understand and agree that we may monitor or review any Content you post as part the Site. We may delete any Content, in whole or in part, that in our sole judgment violates this Agreement or may harm the reputation of the Site.
+
+</p><p>When communicating with our customer care representatives, you agree to be respectful and kind. If we feel that your behavior towards any of our customer care representatives or other employees is at any time threatening or offensive, we reserve the right to immediately terminate your account.
+
+</p><p>You agree that Senior Scramble may access, preserve and disclose your account information and Content if required to do so by law or in a good faith belief that such access, preservation or disclosure is reasonably necessary, such as to: (i) comply with legal process; (ii) enforce this Agreement; (iii) respond to claims that any Content violates the rights of third parties; (iv) respond to your requests for customer service; or (v) protect the rights, property or personal safety of the Company or any other person.
+</p>
+<p>&nbsp;</p>
+
+<h3>8. Community Rules.</h3>
+<p>By using the Site, you agree that you will not:</p>
+<ul>
+<li>Use the Site for any purpose that is illegal or prohibited by this Agreement. 
+</li><li>Use the Site for any harmful or nefarious purpose.
+</li><li>Use the Site in order to damage Senior Scramble.
+</li><li>Spam, solicit money from or defraud any members.
+</li><li>Impersonate any person or entity or post any images of another person without his or her permission.
+</li><li>Bully, “stalk,” intimidate, assault, harass, mistreat or defame any person.
+</li><li>Post any Content that violates or infringes anyone’s rights, including rights of publicity, privacy, copyright, trademark or other intellectual property or contract right.
+</li><li>Post any Content that is hate speech, threatening, sexually explicit or pornographic; incites violence; or contains nudity or graphic or gratuitous violence.
+</li><li>Post any Content that promotes racism, bigotry, hatred or physical harm of any kind against any group or individual.
+</li><li>Solicit passwords for any purpose, or personal identifying information for commercial or unlawful purposes from other users or disseminate another person’s personal information without his or her permission.
+</li><li>Use another user’s account, share an account with another user, or maintain more than one account.
+</li><li>Create another account if we have already terminated your account, unless you have our permission.
+</li></ul>
+<p>Senior Scramble reserves the right to investigate and/ or terminate your account without a refund of any purchases if you have violated this Agreement, misused the Site or behaved in a way that Senior Scramble regards as inappropriate or unlawful, including actions or communications that occur on or off the Site.
+</p>
+<p>&nbsp;</p>
+<h3>9. Other Members’ Content. </h3>
+<p>Although Senior Scramble reserves the right to review and remove Content that violates this Agreement, such Content is the sole responsibility of the member who posts it, and Senior Scramble cannot guarantee that all Content will comply with this Agreement. If you see Content on the Site that violates this Agreement, please contact us.
+</p>
+<p>&nbsp;</p>
+<h3>10. Disclaimers. </h3>
+<p>SENIOR SCRAMBLE PROVIDES THE SITE ON AN “AS IS” AND “AS AVAILABLE” BASIS AND TO THE EXTENT PERMITTED BY APPLICABLE LAW, GRANTS NO WARRANTIES OF ANY KIND, WHETHER EXPRESS, IMPLIED, STATUTORY OR OTHERWISE WITH RESPECT TO THE SITE (INCLUDING ALL CONTENT CONTAINED THEREIN), INCLUDING, WITHOUT LIMITATION, ANY IMPLIED WARRANTIES OF SATISFACTORY QUALITY, MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT. SENIOR SCRAMBLE DOES NOT REPRESENT OR WARRANT THAT (A) THE SITE WILL BE UNINTERRUPTED, SECURE OR ERROR FREE, (B) ANY DEFECTS OR ERRORS IN THE SITE WILL BE CORRECTED, OR (C) THAT ANY CONTENT OR INFORMATION YOU OBTAIN ON OR THROUGH THE SITE WILL BE ACCURATE.
+</p>
+<p>SENIOR SCRAMBLE TAKES NO RESPONSIBILITY FOR ANY CONTENT THAT YOU OR ANOTHER MEMBER POSTS, SENDS OR RECEIVES THROUGH THE SITE. ANY MATERIAL OBTAINED THROUGH THE USE OF THE SITE IS ACCESSED AT YOUR OWN DISCRETION AND RISK. 
+</p>
+<p>SENIOR SCRAMBLE DISCLAIMS AND TAKES NO RESPONSIBILITY FOR ANY CONDUCT OF YOU OR ANY OTHER MEMBER, ON OR OFF THE SITE.
+</p>
+<p>&nbsp;</p>
+<h3>11. Indemnity by You</h3> 
+<p>You agree, to the extent permitted under applicable law, to indemnify, defend and hold harmless Senior, our affiliates, and their and our respective officers, directors, agents, and employees from and against any and all complaints, demands, claims, damages, losses, costs, liabilities and expenses, including attorney’s fees, due to, arising out of, or relating in any way to your access to or use of the Site, your Content, or your breach of this Agreement.
+ </p>
+
+</p>
+</div>
     </div>
   );
 };
